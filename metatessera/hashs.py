fdbae30dcebcb8cf004811a994c277f25393dad3
TrexPD <leynad157@gmail.com>
2023-01-21 18:39:59 -0300

    add os arquivos metatessera e hashs!

---
+++ metatessera/hashs.py
@@ -0,0 +1,35 @@
+from rich.text import Text
+import hashlib
+
+
+def hash_calculation(file_name: str) -> str:
+    with open(f'{file_name}', 'rb') as archive:
+        hash_md5 = hashlib.md5()
+        hash_sha1 = hashlib.sha1()
+        hash_sha224 = hashlib.sha224()
+        hash_sha256 = hashlib.sha256()
+        hash_sha384 = hashlib.sha384()
+        hash_sha512 = hashlib.sha512()
+        while True:
+            data: bytes = archive.read(1024)
+            if not data:
+                break
+            hash_md5.update(data)
+            hash_sha1.update(data)
+            hash_sha224.update(data)
+            hash_sha256.update(data)
+            hash_sha384.update(data)
+            hash_sha512.update(data)
+        return Text(f"""
+[yellow][b]CHECKSUM:[/][/]
+
+[blue]MD5   [/]                            [white][b]: {hash_md5.hexdigest()}[/][/]
+[blue]SHA1  [/]                            [white][b]: {hash_sha1.hexdigest()}[/][/]
+[blue]SHA224[/]                            [white][b]: {hash_sha224.hexdigest()}[/][/]
+[blue]SHA256[/]                            [white][b]: {hash_sha256.hexdigest()}[/][/]
+[blue]SHA384[/]                            [white][b]: {hash_sha384.hexdigest()}[/][/]
+[blue]SHA512[/]                            [white][b]: {hash_sha512.hexdigest()}[/][/]""")
+
+
+if __name__ in "__main__":
+    print(hash_calculation('setup.py'))
---
+++ metatessera/metatessera_en-US.py
@@ -0,0 +1,76 @@
+from time import ctime, strptime, strftime
+from re import sub, findall
+from pathlib import Path
+from hashs import hash_calculation
+from rich import print
+from rich.panel import Panel
+import click
+
+
+def amount_of_characters(text: str) -> int: 
+    return len(text)
+
+
+def amount_of_letters(text: str) -> int:  
+    letters: list = findall(r'[a-zA-ZÀ-ú]', text)
+    return len(letters)
+
+
+def amount_of_words(text: str) -> int:
+    words: list = sub(r"\W+", " ", text)
+    return len(words.split())
+
+
+def amount_of_numbers(text: str) -> int:
+    numbers: list = findall(r'\d', text)
+    return len(numbers)
+
+
+def number_of_lines(file: str) -> int:
+    with open(f"{file}", 'rt', encoding='utf-8') as archive:
+        lines = archive.readlines()
+        return len(lines)
+
+
+@click.command()
+@click.argument('file', type=click.STRING, envvar='METAFILE')
+def main(file: str) -> str:
+    if Path(file).is_file():
+            file_path = Path(file)
+            created = strptime(ctime(file_path.stat().st_ctime))
+            modified = strptime(ctime(file_path.stat().st_mtime))
+            last_access = strptime(ctime(file_path.stat().st_atime))
+            try:
+                with open(file=file, mode='rt', encoding='utf-8') as archive:
+                    read_file: str = archive.read(1024)
+            except FileNotFoundError:
+                click.echo(f"The file {file} was not found!\n")
+            except UnicodeDecodeError:
+                click.echo('Unsupported file, run the command "meta --extensions" to see supported file types!\n')
+            except:
+                click.echo("Sorry, unknown error... :(\n")
+            else:
+                print(
+Panel(f"""[yellow][bold]DETAILS:[/][/]
+
+[blue]File name              [/]           [white][b]: {file_path.stem}                              [/]
+[blue]File path              [/]           [white][b]: {file_path.absolute()}                        [/]
+[blue]File size              [/]           [white][b]: {(int(file_path.stat()[6]) / 1024):.2f} KB    [/]
+[blue]File extension         [/]           [white][b]: {file_path.suffixes}                          [/]
+[blue]No. of characters      [/]           [white][b]: {amount_of_characters(read_file)}             [/]
+[blue]No. of words           [/]           [white][b]: {amount_of_words(read_file)}                  [/]
+[blue]No. of letters         [/]           [white][b]: {amount_of_letters(read_file)}                [/]
+[blue]Quantity of No.        [/]           [white][b]: {amount_of_numbers(read_file)}                [/]
+[blue]Quantity of lines      [/]           [white][b]: {number_of_lines(file)}                       [/]
+[blue]Created (Date/Time)    [/]           [white][b]: {strftime('%d/%m/%Y %X %z', created)}         [/]
+[blue]Modified (Date/Time)   [/]           [white][b]: {strftime('%d/%m/%Y %X %z', modified)}        [/]
+[blue]Last Access (Date/Time)[/]           [white][b]: {strftime('%d/%m/%Y %X %z', last_access)}     [/]
+{hash_calculation(file)}"""))
+
+    elif Path(file).is_dir():
+        click.echo(f'The "{file}" is a directory, and cannot be read!\n')
+    else:
+        click.echo(f'The "{file}" is an incorrect path or a non-existent file!\n')
+
+if __name__ == '__main__':
+    main()
