0e1974b58bef3403c9c1db00a2f85eca0beb2950
TrexPD <leynad157@gmail.com>
2023-01-21 18:45:01 -0300

    add um requerimento para a lib hashlib!

--- metatessera/setup.py
+++ metatessera/setup.py
@@ -12,7 +12,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'click', 'rich'
+        'click', 'rich', 'hashlib'
     ],
     entry_points="""
     [console_scripts]
